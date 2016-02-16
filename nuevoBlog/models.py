b6de854854a47acd889e4c624854ee4a3047ce29
Juan <jcpablo10@gmail.com>
2016-02-16 11:23:24 -0600

    Hasta aqui funciona el nuevo modelo

---
+++ nuevoBlog/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.template.defaultfilters import slugify
+
+# Create your models here.
+
+class Articulo(models.Model):
+	titulo = models.CharField(max_length=200)
+	contenido = models.TextField()
+	imagen = models.ImageField(upload_to="nuevoBlog/images-articles/")
+	slug = models.SlugField(editable=False)
+	fecha = models.DateField()
+
+	def __unicode__(self):
+		return self.titulo
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.titulo)
+		super(Articulo, self).save(*args, **kwargs)
